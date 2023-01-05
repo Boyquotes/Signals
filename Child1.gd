2b73b05a64e632daac6a014802c337d9fa943785
ill13 <ill13@users.noreply.github.com>
2023-01-04 21:54:18 -0500

    lol

--- Child1.gd
+++ Child1.gd
@@ -1,14 +1,8 @@
 extends Node
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
-	yield(get_tree().create_timer(0.01), "timeout")
+	#yield(get_tree().create_timer(0.01), "timeout")
 	print("Child1")
 	
